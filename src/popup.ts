b1cd7ff76f2002daa8de409f378379fabaa245ee
hsnavarro <ricksnavarro@gmail.com>
2020-11-18 22:36:45 -0300

    Add types to popup.ts

--- src/popup.ts
+++ src/popup.ts
@@ -15,79 +15,75 @@ declare global {
   }
 }
 
-const background = chrome.extension.getBackgroundPage();
+const background: Window = chrome.extension.getBackgroundPage();
 
-let connected = false;
+const createRoomButton: HTMLButtonElement = document.getElementById('createRoom') as HTMLButtonElement;
+const copyUrlButton: HTMLButtonElement = document.getElementById('copyUrl') as HTMLButtonElement;
+const disconnectButton: HTMLButtonElement = document.getElementById('disconnect') as HTMLButtonElement;
+const urlInput: HTMLInputElement = document.getElementById('urlInput') as HTMLInputElement;
+let optionButtons: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByClassName('actionButton') as HTMLCollectionOf<HTMLButtonElement>;
 
-const createRoomButton = document.getElementById('createRoom');
-const copyUrlButton = document.getElementById('copyUrl');
-const disconnectButton = document.getElementById('disconnect');
-const urlInput = document.getElementById('urlInput') as HTMLInputElement;
-let optionButtons = document.getElementsByClassName('actionButton');
-
-getExtensionColor().then((color: string) => {
+getExtensionColor().then((color: string): void => {
   for (let button of optionButtons) {
-    const buttonHTMLElement = button as HTMLElement;
     log("Color of " + button.id + " is now " + color);
-    buttonHTMLElement.style.backgroundColor = color;
+    button.style.backgroundColor = color;
   }
 });
 
-function executeScript(tabId, obj) {
+/*
+function executeScript(tabId, obj): Promise<void[]> {
   return new Promise(
-    callback => chrome.tabs.executeScript(tabId, obj, callback)
-  );
+    resolve => chrome.tabs.executeScript(tabId, obj, resolve)
+    );
 }
+*/
 
 function update() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const tab = tabs[0];
-    const roomId = background.window.getRoomId(tab.id);
-    const connected = roomId != null;
+    const tab: chrome.tabs.Tab = tabs[0];
+    const roomId: string = background.window.getRoomId(tab.id);
+    const connected: boolean = roomId != null;
 
     log('Updating Popup...', { roomId, connected });
 
     if (connected) {
-      const url = updateQueryStringParameter(tab.url, 'rollTogetherRoom', roomId)
+      const url: string = updateQueryStringParameter(tab.url, 'rollTogetherRoom', roomId)
       urlInput.value = url;
       urlInput.focus();
       urlInput.select();
 
-      [...document.getElementsByClassName('firstPage')].forEach((el: HTMLElement) => el.style.display = 'none');
-      [...document.getElementsByClassName('secondPage')].forEach((el: HTMLElement) => el.style.display = 'flex');
+      [...document.getElementsByClassName('firstPage')].forEach((el: HTMLElement): void => { el.style.display = 'none' });
+      [...document.getElementsByClassName('secondPage')].forEach((el: HTMLElement): void => { el.style.display = 'flex' });
     } else {
-      [...document.getElementsByClassName('firstPage')].forEach((el: HTMLElement) => el.style.display = 'flex');
-      [...document.getElementsByClassName('secondPage')].forEach((el: HTMLElement) => el.style.display = 'none');
+      [...document.getElementsByClassName('firstPage')].forEach((el: HTMLElement): void => { el.style.display = 'flex' });
+      [...document.getElementsByClassName('secondPage')].forEach((el: HTMLElement): void => { el.style.display = 'none' });
     }
   });
 }
 
-window.addEventListener('beforeunload', () => {
+window.addEventListener('beforeunload', (): void => {
   background.window.updatePopup = null;
 });
 
-createRoomButton.onclick = async function () {
+createRoomButton.onclick = async function (): Promise<void> {
   log('Clicking CreateRoomButton');
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs: chrome.tabs.Tab[]): void {
     background.window.createRoom(tabs[0]);
   });
 };
 
-copyUrlButton.onclick = function () {
+copyUrlButton.onclick = function (): void {
   log('Clicking CopyUrlButton');
   urlInput.focus();
   urlInput.select();
   document.execCommand('copy');
 }
 
-disconnectButton.onclick = function () {
+disconnectButton.onclick = function (): void {
   log('Clicking DisconnectButton');
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs: chrome.tabs.Tab[]): void {
     background.window.disconnectRoom(tabs[0].id);
   })
-}
-
-urlInput.onclick = function () {
 }
 
 background.window.updatePopup = update;
